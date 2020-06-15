923835015bd44fc4c55041ee27846d66cc32bbe1
Alper Gokcehan <mail@alperg.com>
2020-06-15 16:40:34 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const request = require('request');
+const PORT = process.env.PORT || 8090;
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/auth/redirect', (req, res) =>{
+    const options = {
+        uri: 'https://slack.com/api/oauth.access?code='
+            +req.query.code+
+            '&client_id='+process.env.CLIENT_ID+
+            '&client_secret='+process.env.CLIENT_SECRET+
+            '&redirect_uri='+process.env.REDIRECT_URI,
+        method: 'GET'
+    };
+    
+    request(options, (error, response, body) => {
+        const JSONresponse = JSON.parse(body);
+        
+        if (!JSONresponse.ok){
+            console.log(JSONresponse);
+            res.send("Error encountered: \n" + JSON.stringify(JSONresponse)).status(200).end();
+        } else {
+            console.log(JSONresponse);
+            res.send("Success!");
+        }
+    });
+});
+
+app.listen(PORT, function() {
+  console.log(`App listening on PORT: ${PORT}`);
+});
